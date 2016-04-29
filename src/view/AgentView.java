0742e4b039f66c505433c05b5a870ae00953b68a
jaw233 <jaw233@txstate.edu>
2016-04-29 15:28:14 -0500

    o hai :)

--- src/view/AgentView.java
+++ src/view/AgentView.java
@@ -1,17 +1,62 @@
 package view;
 
 
+import controller.AgentController;
 import controller.BankingController;
+import controller.Controller;
+import model.AccountModel;
 import model.Model;
 import model.ModelEvent;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AgentView extends JFrameView {
-    public static final String AgentID = "";
+    AccountModel current_account;
+    double currency_rate;
+    public JTextField input_amount = new JTextField(20);
+    public JTextField ops_per_second = new JTextField(20);
+    public JTextField state = new JTextField(15);
+    public JTextField amount_transferred = new JTextField(15);
+    public JTextField ops_completed = new JTextField(15);
+
+    public static final String stop = "Stop Agent";
+    public static final String dismiss = "Dismiss";
 
     public AgentView(Model model, AgentController controller, AccountModel acc, double rate){
+        super(model, (Controller) controller);
+        this.currency_rate = rate;
+        this.current_account = acc;
+
+        JPanel panel = new JPanel();
+        Jbutton_handler button_hander = new Jbutton_handler();
+
+        panel.add(input_amount);
+        panel.add(ops_per_second);
+        panel.add(state);
+        panel.add(amount_transferred);
+        panel.add(ops_completed);
+
+        input_amount.setText(Double.toString(current_account.getBalance()));
+        ops_per_second.setText("0");
+        state.setText("Running");
+        amount_transferred.setText("0");
+        ops_completed.setText("0");
+
+        input_amount.setEditable(false);
+        ops_per_second.setEditable(false);
+        state.setEditable(false);
+        amount_transferred.setEditable(false);
+        ops_completed.setEditable(false);
+
+        JButton stop_button = new JButton(stop);
+        JButton dismiss_button = new JButton(dismiss);
+
+        panel.setLayout(new GridLayout(5,1));
+        this.getContentPane().add(panel, BorderLayout.CENTER);
+
 
     }
 
