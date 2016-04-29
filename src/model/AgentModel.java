7fbade1ee0ab41143465b1b80affc7f7acbd58b1
Sean Thornton <st1142@gmail.com>
2016-04-29 16:20:16 -0500

    agent model default constructor

--- src/model/AgentModel.java
+++ src/model/AgentModel.java
@@ -1,6 +1,17 @@
 package model;
 
-public class AgentModel extends AbstractModel {
+public class AgentModel extends Thread {
 
+    public AccountModel current_account;
+    public double amount;
+    public boolean running = false;
+    public double operations;
+
+
+    public AgentModel (AccountModel acc, double o, double a) {
+        this.current_account = acc;
+        this.amount = a;
+        this.operations = o;
+    }
 
 }
