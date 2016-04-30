7b50fe9bbf55671b0936d85e0d2c347ec10c604e
Sean Thornton <st1142@gmail.com>
2016-04-29 20:14:31 -0500

    AgentModel cleaned up

--- src/model/AgentModel.java
+++ src/model/AgentModel.java
@@ -1,36 +1,17 @@
 package model;
 
-import view.AgentView;
-import view.JFrameView;
-
-import javax.swing.*;
-
 public class AgentModel extends AbstractModel {
 
     private int agent_id;
     private AccountModel current_account;
-    private double amount;
-    private double amount_transferred;
-    private boolean running = false;
-    private String operation;
-    private double operations_per_second = 0;
-    private int operations_completed = 0;
-
 
     public AgentModel (int id, AccountModel acc, double n, double a, String o) {
-
         this.agent_id = id;
         this.current_account = acc;
-        this.amount = a;
-        this.operations_per_second = n;
-        this.operation = o;
     }
 
-    public int getAgent_id() {
-        return agent_id;
-    }
-
-    public void setAgent_id(int agent_id) {
-        this.agent_id = agent_id;
-    }
+    public int getAgent_id() { return agent_id; }
+    public void setAgent_id(int agent_id) { this.agent_id = agent_id; }
+    public AccountModel getCurrent_account() { return current_account; }
+    public void setCurrent_account(AccountModel current_account) { this.current_account = current_account; }
 }
